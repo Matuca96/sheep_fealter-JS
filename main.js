8c5dee854fb44c89873d2c880fcca5d99cc337b0
Matuca96 <matucagatao@gmail.com>
2025-05-26 16:41:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// string
+
+const firstName = "jhon";
+const lastName = "doe";
+
+const names = "jhon,doe,mateus";
+
+
+console.log(`meu nome e ${firstName} ${lastName.
+toLowercase()}`);
+
+console.log(names.split(","));
